2996389902485d8941ff71a9ceeb53860d1e33b9
Fernanda Panca Prima <pancaprima8@gmail.com>
2017-11-21 20:14:07 +0700

    initial package (+1 squashed commit) Squashed commits: [b92ae66] add git ignore

---
+++ renderer.js
@@ -0,0 +1,9 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+$('#goto').on('click', (e) => {
+  e.preventDefault()
+  $('#intro').hide()
+  $('nav').show()
+})
