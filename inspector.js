3ab52e3be49cf2eddad8f78ca385d4bcecd1edc7
Fernanda Panca Prima <pancaprima8@gmail.com>
2018-01-21 18:17:42 +0700

    move inspector logic to smart-selector.js file

--- inspector.js
+++ inspector.js
@@ -1,186 +1,5 @@
-/**
- * Simple JavaScript DOM Inspector v0.1.2
- *
- * Highlights hovered elements with a 2px red outline, and then provides the hovered element
- * on click to the callback function, which can do anything with it.
- *
- * By default, the inspector just console.logs a jQuery-style CSS selector path for the element.
- * 
- * The CSS path-building code works up the given element's parent nodes to create an optimised
- * selector string, stopping at the first parent with an #id, but can also create an
- * ultra-specific full CSS path, right down to 'html'.
- * 
- * Optionally, it can check to see if any part of the CSS path matches multiple elements, or if
- * any element in the CSS path has no #id or .class, and add specific ":nth-child()"
- * pseudo-selector to match the element, eg. "html body #content p img:nth-child(1)"
- * 
- * Hit escape key to cancel the inspector.
- * 
- * NB: XPath code removed as it didn't really work very well, need to write from scratch.
- * 
- * Started putting in IE support, but won't work in IE just yet, check back next week for that
- * (so far, tested in FF4, Chrome, Safari, Opera 11.)
- * 
- * No warranty; probably won't break the internet. Improvements and linkbacks welcome!
- * 
- * - Joss
- */
-
-
 var last;
-var page;
-
-/**
-* Get full CSS path of any element
-* 
-* Returns a jQuery-style CSS path, with IDs, classes and ':nth-child' pseudo-selectors.
-* 
-* Can either build a full CSS path, from 'html' all the way to ':nth-child()', or a
-* more optimised short path, stopping at the first parent with a specific ID,
-* eg. "#content .top p" instead of "html body #main #content .top p:nth-child(3)"
-*/
-function cssPath(el) {
-  let fullPath    = 0,  // Set to 1 to build ultra-specific full CSS-path, or 0 for optimised selector
-      useNthChild = 0,  // Set to 1 to use ":nth-child()" pseudo-selectors to match the given element
-      specialAttributes = ['name', 'href', 'value'],
-      currentElement = el,
-      cssPathStr = '',
-      testPath = '',
-      parents = [],
-      parentSelectors = [],
-      attributes = [],
-      tagName,
-      cssId,
-      cssClass,
-      tagSelector,
-      vagueMatch,
-      nth,
-      i,
-      c;
-
-  // Go up the list of parent nodes and build unique identifier for each:
-  while ( currentElement ) {
-    vagueMatch = 0;
-
-    // Get the node's HTML tag name in lowercase:
-    tagName = currentElement.nodeName.toLowerCase();
-    
-    // Get node's ID attribute, adding a '#':
-    cssId = ( currentElement.id ) ? ( '#' + currentElement.id ) : false;
-    
-    // Get node's CSS classes, replacing spaces with '.':
-    cssClass = ( currentElement.className ) ? ( '.' + currentElement.className.replace(/\s+/g,".") ) : '';
-    
-    // Get special attributes from this node
-    let hasSpecialAttribute = false
-    let elementSpecialAttributes = []
-    for (let i in specialAttributes) {
-      let attrValue
-      if ( attrValue = currentElement.getAttribute(specialAttributes[i]) ) { 
-        elementSpecialAttributes.push({ 
-          'name' : specialAttributes[i],
-          'value': attrValue
-        })
-        hasSpecialAttribute = true
-      }
-    }
-
-    // Try build a unique identifier for this parent node by ID
-    if ( cssId ) {
-      // Matched by ID:
-      tagSelector = tagName + cssId + cssClass;
-    } else if ( cssClass ) {
-      tagSelector = tagName + cssClass;
-    } else {
-      vagueMatch = 1
-      tagSelector = tagName
-    }
-
-    // Try build a unique identifier for this parent node by attributes
-    let specialAttributesVague = 1
-    if ( hasSpecialAttribute ) {
-      result = checkWithAttributes(currentElement, tagName, tagSelector, elementSpecialAttributes)
-      vagueMatch = result.vague
-      specialAttributesVague = result.vague
-      tagSelector = result.tagSelector
-    }
-
-    if (tagName == "a") {
-      parentSelectors = []
-      el = currentElement
-    }
-
-    // Add this full tag selector to the parentSelectors array:
-    parentSelectors.push( tagSelector )
-
-    // If doing short/optimised CSS paths and this element has an ID, stop here:
-    if ( cssId && !fullPath )
-      break;
-    
-    // Go up to the next parent node:
-    currentElement = currentElement.parentNode !== page ? currentElement.parentNode : false;
-    
-  } // endwhile
-
-  // Build the CSS path string from the parent tag selectors:
-  let minimumValidSelector = ''
-  let finalCssPath = ''
-  for ( i = 0; i < parentSelectors.length; i++ ) {
-    cssPathStr = parentSelectors[i] + ' ' + cssPathStr;// + ' ' + cssPathStr;
-
-    // If using ":nth-child()" selectors and this selector has no ID / isn't the html or body tag:
-    if ( useNthChild && !parentSelectors[i].match(/#/) && !parentSelectors[i].match(/^(html|body)$/) ) {
-      
-      // If there's no CSS class, or if the semi-complete CSS selector path matches multiple elements:
-      if ( !parentSelectors[i].match(/\./) || $( cssPathStr ).length > 1 ) {
-        
-        // Count element's previous siblings for ":nth-child" pseudo-selector:
-        for ( nth = 1, c = currentElement; c.previousElementSibling; c = c.previousElementSibling, nth++ );
-        
-        // Append ":nth-child()" to CSS path:
-        cssPathStr += ":nth-child(" + nth + ")";
-      }
-    }
-    finalCssPath = cssPathStr.replace(/^[ \t]+|[ \t]+$/, '')
-    if (el.isSameNode(document.querySelector(finalCssPath))) {
-      if (minimumValidSelector == '') minimumValidSelector = finalCssPath
-      if (document.querySelectorAll(finalCssPath).length == 1) {
-        minimumValidSelector = finalCssPath
-        break
-      }
-    }
-  }
-  if (minimumValidSelector == '') console.warn("can't generate selector for this element", el, finalCssPath )
-
-  return minimumValidSelector;
-}
-
-function checkWithAttributes (el, tagName, tagSelector, elementSpecialAttributes) {
-  let attrQuery = ''
-  let vague = 1
-  let finalTagSelector = ''
-  let iteratorCounter = (tagName == tagSelector) ? 1 : 2
-  for (let i=0; i < iteratorCounter; i++) {
-    tempTagSelector = (iteratorCounter == 1) ? tagName : tagSelector
-    for (let j in elementSpecialAttributes) {
-      let eAttr = elementSpecialAttributes[j]
-      attrQuery = attrQuery + "[" + eAttr.name + "=" + "'" + eAttr.value + "'" +"]"
-      finalTagSelector = tempTagSelector + attrQuery
-      if ( el.isSameNode(document.querySelector(finalTagSelector)) ) {
-        vague = 0
-        break
-      }
-    }
-    if (!vague) break
-     else finalTagSelector = tagSelector
-  }
-  
-  return {
-    "vague" : vague,
-    "tagSelector" : finalTagSelector
-  }
-}
-
+var smartSelector = require('./smart-selector');
 
 /**
 * MouseOver action for all elements on the page:
@@ -212,7 +31,7 @@ function inspectorMouseOut(e) {
 function inspectorOnClick(e) {
   e.preventDefault();
 
-  return cssPath(e.target);
+  return smartSelector.findElementLocation(e.target, smartSelector.SHORTEST_STRATEGY)
 }
 
 
@@ -221,12 +40,7 @@ function inspectorOnClick(e) {
 */
 function inspectorCancel() {
   // Unbind inspector mouse and click events:
-  page = null
   last.style.outline = 'none'
-}
-
-function inspectorStart(doc) {
-  page = doc
 }
 
 module.exports = { inspectorMouseOver, inspectorMouseOut, inspectorOnClick, inspectorCancel, inspectorStart};
---
+++ smart-selector.js
@@ -0,0 +1,194 @@
+const XPATH_STRATEGY = "XPATH"
+const CSS_SELECTOR_STRATEGY = "CSS_SELECTOR"
+const SHORTEST_STRATEGY = "SHORTEST"
+
+function findElementLocation(el, strategy) {
+  let location
+  switch (strategy) {
+    case SHORTEST_STRATEGY:
+      location = getShortestLocation(el)
+      break;
+    case CSS_SELECTOR_STRATEGY:
+      location = getCSSLocation(el)
+      break;
+    case XPATH_STRATEGY:
+      location = getXPathLocation(el)
+      break;
+    default:
+      break;
+  }
+  return location
+}
+
+function getShortestLocation(el) {
+  el = validateElement(el)
+  let strategy = SHORTEST_STRATEGY,
+      css = {
+        "parents" : getAllElementLocations(el, CSS_SELECTOR_STRATEGY),
+        "location": '',
+        "minimum_valid": null
+      },
+      xpath = {
+        "parents" : getAllElementLocations(el, XPATH_STRATEGY),
+        "location": '',
+        "minimum_valid": null,
+        "minimum_valid_index" : Number.MAX_SAFE_INTEGER
+      }
+
+  for (let i = 0; i < css.parents.length; i++ ) {
+    css.location = css.parents[i] + ' ' + css.location,
+    xpath.location = '/' + xpath.parents[i] + xpath.location
+    // If using ":nth-child()" selectors and this selector has no ID / isn't the html or body tag:
+    if ( !css.parents[i].match(/#/) && !css.parents[i].match(/^(html|body)$/) ) {
+      // If there's no CSS class, or if the semi-complete CSS selector path matches multiple elements:
+      if ( !css.parents[i].match(/\./) || $( css.location ).length > 1 ) {
+        // Count element's previous siblings for ":nth-child" pseudo-selector:
+        let nth
+        for ( nth = 1, c = el; c.previousElementSibling; c = c.previousElementSibling, nth++ );
+        // Append ":nth-child()" to CSS path:
+        if (nth > 1) css.location = css.location.trim() + ":nth-child(" + nth + ")";
+      }
+    }
+    css.location = css.location.replace(/^[ \t]+|[ \t]+$/, '')
+    let cssNodes = document.querySelectorAll(css.location)
+    for (let j=0; j < cssNodes.length; j++) {
+      if ( j==0 && el.isSameNode(cssNodes[j]) ) {
+          css.minimum_valid = css.location
+          break
+      } else {
+          let xpathNodes = document.evaluate('/' + xpath.location, document, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null)
+          let xpathNode = xpathNodes.iterateNext()
+          let nodeIteration = 1
+          while (xpathNode) {
+            if (nodeIteration < xpath.minimum_valid_index && el.isSameNode(xpathNode)) {
+              xpath.minimum_valid = (nodeIteration==1) ? "/" + xpath.location : "(/" + xpath.location +")["+nodeIteration+"]"
+              break
+            }
+            nodeIteration++
+            xpathNode = xpathNodes.iterateNext()
+          }
+      }
+    }
+    if (css.minimum_valid) break
+  }
+  
+  if (css.minimum_valid) {
+    if (xpath.minimum_valid) {
+      if (xpath.minimum_valid.length > css.minimum_valid.length) return css.minimum_valid
+    } else return css.minimum_valid
+  } 
+  return xpath.minimum_valid
+}
+
+function getCSSLocation(el) {
+  //to-do next
+  return null
+}
+
+function getXPathLocation(el) {
+  //to-do next
+  return null
+}
+
+function validateElement(el) {
+  let currentElement = el
+  while ( currentElement ) {
+    if (currentElement.nodeName.toLowerCase() == "a") el = currentElement
+    currentElement = currentElement.parentNode !== document ? currentElement.parentNode : false;
+  }
+  return el
+}
+
+function getAllElementLocations(el, strategy) {
+  let currentElement = el,
+      parentElementLocations = []
+
+  // Go up the list of parent nodes and build unique identifier for each:
+  while ( currentElement ) {
+    // Get the node's HTML tag name in lowercase:
+    let tagName = currentElement.nodeName.toLowerCase()
+    let tagId, tagClasses
+    switch (strategy) {
+      case CSS_SELECTOR_STRATEGY:
+        tagId = ( currentElement.id ) ? ( '#' + currentElement.id ) : false
+        tagClasses = ( currentElement.className ) ? ( '.' + currentElement.className.replace(/\s+/g,".") ) : ''
+        break;
+      case XPATH_STRATEGY:
+        tagId = ( currentElement.id ) ? ( "[@id='" + currentElement.id + "']" ) : false
+        tagClasses = ( currentElement.className ) ? ( "[@class='" + currentElement.className + "']" ) : ''
+      default:
+        break;
+    }
+ 
+    let elementLocation
+    if ( tagId ) elementLocation = tagName + tagId
+    else if ( tagClasses ) elementLocation = tagName + tagClasses
+    else elementLocation = tagName
+
+    // Try build a unique identifier for this parent node by attributes
+    let specialAttributes = getSpecialAttributes(currentElement)
+    if ( specialAttributes.has_special_attributes ) elementLocation = checkWithAttributes(currentElement, strategy, tagName, elementLocation, specialAttributes.data)
+
+    // Add this element location to the parentElementLocations array:
+    parentElementLocations.push( elementLocation )
+
+    // If this element has an ID, stop here:
+    if ( tagId ) break;
+    
+    // Go up to the next parent node:
+    currentElement = currentElement.parentNode !== document ? currentElement.parentNode : false;
+  } // endwhile
+
+  return parentElementLocations
+}
+
+function getSpecialAttributes(el) {
+  // Get special attributes from this node
+  let specialAttributes = ['name', 'href', 'value'],
+      hasSpecialAttribute = false,
+      elementSpecialAttributes = []
+  
+  for (let i in specialAttributes) {
+    let attrValue
+    if ( attrValue = el.getAttribute(specialAttributes[i]) ) { 
+      elementSpecialAttributes.push({ 
+        'name' : specialAttributes[i],
+        'value': attrValue
+      })
+      hasSpecialAttribute = true
+    }
+  }
+  return {
+    "has_special_attributes" : true,
+    "data" : elementSpecialAttributes
+  }
+}
+
+function checkWithAttributes (el, strategy, tagName, currentLocation, elementSpecialAttributes) {
+  let finalLocation = currentLocation,
+      iteratorCounter = (tagName == currentLocation) ? 1 : 2 //iterate through tagName and currentLocation to get best location
+  for (let i=0; i < iteratorCounter; i++) {
+    let tempLocation = (iteratorCounter == 1) ? tagName : currentLocation,
+        attr = ''
+    for (let j in elementSpecialAttributes) {
+      let eAttr = elementSpecialAttributes[j]
+      switch (strategy) {
+        case CSS_SELECTOR_STRATEGY:
+          attr = attr + "[" + eAttr.name + "=" + "'" + eAttr.value + "'" +"]"
+          if ( el.isSameNode(document.querySelector(tempLocation + attr)) ) finalLocation = tempLocation + attr
+          break
+        case XPATH_STRATEGY:
+          attr = attr + "[@" + eAttr.name + "=" + "'" + eAttr.value + "'" +"]"
+          let xpathNode = document.evaluate('//' + tempLocation + attr, document, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null).iterateNext()
+          if (el.isSameNode(xpathNode)) finalLocation = tempLocation + attr
+          break
+        default:
+          break
+      }
+      if (finalLocation != currentLocation) break
+    }
+  }
+  return finalLocation
+}
+
+module.exports = { findElementLocation, XPATH_STRATEGY, CSS_SELECTOR_STRATEGY, SHORTEST_STRATEGY }
